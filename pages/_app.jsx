acf5fdf92ec37106f3199cbfc96854bcf98e58cd
jhood <jhood@yext.com>
2022-07-09 23:25:08 -0400

    WIP: netlify CMS

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,6 +4,5 @@
 import '../styles/global.css';
 
 export default function App({ Component, pageProps }) {
-  console.log(pageProps);
   return <Component {...pageProps} />;
 }
---
+++ posts/test.md
@@ -0,0 +1,8 @@
+---
+title: test
+date: 2022-05-22
+---
+# Blazin
+## Fast
+### Right
+###### hehe
